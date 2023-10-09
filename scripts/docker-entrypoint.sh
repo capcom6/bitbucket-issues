d824dc698ccd0c161378c4dfdd2f7cc76aec2e47
Aleksandr Soloshenko <capcom@soft-c.ru>
2023-10-09 10:03:44 +0700

    Added: use of gevent

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 PORT=${PORT:-8000}
 
-gunicorn --bind=0.0.0.0:${PORT} --access-logfile=- --proxy-allow-from='*' -k uvicorn.workers.UvicornWorker app.api:server $@
+gunicorn --bind=0.0.0.0:${PORT} --access-logfile=- --proxy-allow-from='*' --timeout 120 -k gevent app.api:server $@
